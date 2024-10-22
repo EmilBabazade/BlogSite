7ee7f79a5e00fd50e77b989f1a7c5334858ba950
Emil Babazade <emil.babazade@atea.com>
2024-10-22 16:08:19 +0300

    Add project files.

---
+++ BlogSite/Controllers/BlogController.cs
@@ -0,0 +1,38 @@
+﻿using BlogSite.Data;
+using BlogSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite.Controllers;
+public class BlogController : Controller
+{
+    private readonly BlogContext _dbContext;
+
+    public BlogController(BlogContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public IActionResult Index()
+    {
+        var posts = _dbContext.BlogPosts.ToList();
+        return View(posts);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(BlogPost blogPost)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.BlogPosts.Add(blogPost);
+            _dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(blogPost);
+    }
+}
---
+++ BlogSite/Data/BlogContext.cs
@@ -0,0 +1,12 @@
+﻿using BlogSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite.Data;
+
+public class BlogContext : DbContext
+{
+    public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+
+}
