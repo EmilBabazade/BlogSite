ad2a6c3f732f0bb0983e5c258f778984b2932310
Emil Babazade <emil.babazade@atea.com>
2024-10-22 16:25:02 +0300

    blogpost crud

--- BlogSite/Controllers/BlogController.cs
+++ BlogSite/Controllers/BlogController.cs
@@ -6,33 +6,138 @@ using Microsoft.EntityFrameworkCore;
 namespace BlogSite.Controllers;
 public class BlogController : Controller
 {
-    private readonly BlogContext _dbContext;
+    private readonly BlogContext _context;
 
-    public BlogController(BlogContext dbContext)
+    public BlogController(BlogContext context)
     {
-        _dbContext = dbContext;
-    }
-    public IActionResult Index()
-    {
-        var posts = _dbContext.BlogPosts.ToList();
-        return View(posts);
+        _context = context;
     }
 
-    [HttpGet]
+    // GET: BlogPosts
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.BlogPosts.ToListAsync());
+    }
+
+    // GET: BlogPosts/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var blogPost = await _context.BlogPosts
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (blogPost == null)
+        {
+            return NotFound();
+        }
+
+        return View(blogPost);
+    }
+
+    // GET: BlogPosts/Create
     public IActionResult Create()
     {
         return View();
     }
 
+    // POST: BlogPosts/Create
     [HttpPost]
-    public IActionResult Create(BlogPost blogPost)
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Title,Content,CreatedAt")] BlogPost blogPost)
     {
         if (ModelState.IsValid)
         {
-            _dbContext.BlogPosts.Add(blogPost);
-            _dbContext.SaveChanges();
+            blogPost.CreatedAt = DateTime.Now; // Set created date
+            _context.Add(blogPost);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
         return View(blogPost);
+    }
+
+    // GET: BlogPosts/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var blogPost = await _context.BlogPosts.FindAsync(id);
+        if (blogPost == null)
+        {
+            return NotFound();
+        }
+        return View(blogPost);
+    }
+
+    // POST: BlogPosts/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,CreatedAt")] BlogPost blogPost)
+    {
+        if (id != blogPost.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(blogPost);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogPostExists(blogPost.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(blogPost);
+    }
+
+    // GET: BlogPosts/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var blogPost = await _context.BlogPosts
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (blogPost == null)
+        {
+            return NotFound();
+        }
+
+        return View(blogPost);
+    }
+
+    // POST: BlogPosts/Delete/5
+    [HttpPost, ActionName("DeleteConfirmed")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var blogPost = await _context.BlogPosts.FindAsync(id);
+        _context.BlogPosts.Remove(blogPost);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool BlogPostExists(int id)
+    {
+        return _context.BlogPosts.Any(e => e.Id == id);
     }
 }
---
+++ BlogSite/Views/Blog/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogSite.Views.Blog
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
